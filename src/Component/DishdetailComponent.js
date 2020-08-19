37ab3dd48bf3b68931e3b9c94a76cfe07241b701
Ronak <ronak.vadhaiya@somaiya.edu>
2020-08-19 12:31:04 +0530

    New Update

---
+++ src/Component/DishdetailComponent.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderDish(dish){
+        if (dish != null) 
+            return (
+                <Card>
+                    <CardImg src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardBody>{dish.description}</CardBody>
+                    </CardBody>
+                </Card>
+            )
+        else 
+            return (
+                <div></div>
+            )        
+    }
+
+    render() {
+
+        return (
+            <div className="row">
+                <div className="col-12 col-md-6 m-1">
+                    {this.renderDish(this.props.selectedDish)}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DishDetail;
